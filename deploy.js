32dd086617d5a3d335dd0b19e203b6ccbce86ff1
Eduardo Lopes <educarvalholopes98@gmail.com>
2019-06-02 02:05:34 -0400

    fix

--- deploy.js
+++ deploy.js
@@ -3,11 +3,8 @@ console.log('Making deploy!');
 var ghpages = require('gh-pages');
 
 ghpages.publish('dist', {
-  branch: 'gh-pages',
-  repo: 'git@github.com:cloped/tRex.git',
+  repo: 'https://github.com/cloped/tRex.git',
   add: true,
-  message: 'auto-generated commit deploy',
-  push: true,
 }, function(err) {
   if(!err){
     console.log('Deploy done!');
